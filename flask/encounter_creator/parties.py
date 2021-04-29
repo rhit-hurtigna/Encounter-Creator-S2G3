3ece6c0b704844c085cdc8d93990eab2cb51df06
rhit-hurtigna <73034845+rhit-hurtigna@users.noreply.github.com>
2021-04-29 16:25:45 -0400

    Party Creation is handled through modal

--- flask/encounter_creator/parties.py
+++ flask/encounter_creator/parties.py
@@ -22,7 +22,6 @@ def parties():
     parties_list = []
     cursor = get_cursor()
     for row in g.user['parties']:
-        print(row.cursor_description)
         party_id = row.ID
         name = row.name
         if party_id is None:
@@ -58,45 +57,43 @@ def parties():
     return render_template('parties/index.html', parties=parties_list)
 
 
-@bp.route('/create', methods=('GET', 'POST'))
+@bp.route('/create', methods=['POST'])
 @login_required
 def create():
-    if request.method == 'POST':
-        name = request.form['name']
-        error = None
-        cursor = get_cursor()
+    name = request.form['name']
+    error = None
+    cursor = get_cursor()
 
-        if not name:
-            error = 'Party name is required.'
-        elif len(name) > 30:
-            error = 'Party name too long - maximum 30 characters.'
-        elif not cursor.execute("SELECT ID FROM DM WHERE ID = ?", session.get('user_id')).fetchone():
-            print("Somehow we dropped the DM from the table mid-session?")
+    if not name:
+        error = 'Party name is required.'
+    elif len(name) > 30:
+        error = 'Party name too long - maximum 30 characters.'
+    elif not cursor.execute("SELECT ID FROM DM WHERE ID = ?", session.get('user_id')).fetchone():
+        print("Somehow we dropped the DM from the table mid-session?")
+        return redirect(url_for('auth.logout'))
+
+    if error is None:
+        cursor.execute("DECLARE @Status SMALLINT "
+                       "EXEC @Status = create_party @ID_1=?, @Name_2=? "
+                       "SELECT @Status AS status",
+                       session.get('user_id'), name)
+        status = cursor.fetchval()
+
+        if status == 0:
+            cursor.commit()
+            return redirect(url_for('parties.parties'))
+        elif status == 1:
+            print("Somehow, the DMID is null? Logging out")
             return redirect(url_for('auth.logout'))
+        elif status == 2:
+            print("Safety check for nonexistent DM should have triggered earlier in party create view!")
+            return redirect(url_for('auth.logout'))
+        elif status == 3:
+            print("Safety check for null party name should have triggered earlier in party create view!")
+            error = 'Party name is required.'
+        else:
+            print("Unknown error code in create_party:", status)
+            error = 'Server error - try again?'
 
-        if error is None:
-            cursor.execute("DECLARE @Status SMALLINT "
-                           "EXEC @Status = create_party @ID_1=?, @Name_2=? "
-                           "SELECT @Status AS status",
-                           session.get('user_id'), name)
-            status = cursor.fetchval()
-
-            if status == 0:
-                cursor.commit()
-                return redirect(url_for('parties.parties'))
-            elif status == 1:
-                print("Somehow, the DMID is null? Logging out")
-                return redirect(url_for('auth.logout'))
-            elif status == 2:
-                print("Safety check for nonexistent DM should have triggered earlier in party create view!")
-                return redirect(url_for('auth.logout'))
-            elif status == 3:
-                print("Safety check for null party name should have triggered earlier in party create view!")
-                error = 'Party name is required.'
-            else:
-                print("Unknown error code in create_party:", status)
-                error = 'Server error - try again?'
-
-        flash(error)
-
-    return render_template('parties/create.html')
+    flash(error)
+    return redirect(url_for('parties.parties'))
