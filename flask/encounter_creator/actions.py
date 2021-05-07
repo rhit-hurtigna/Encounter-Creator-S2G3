ffb84509a0610fbace9f86e6720726d7bc72f83b
rhit-hurtigna <73034845+rhit-hurtigna@users.noreply.github.com>
2021-05-06 20:32:01 -0400

    Basic HTML for actions page

--- flask/encounter_creator/actions.py
+++ flask/encounter_creator/actions.py
@@ -15,7 +15,29 @@ from .helpers import alignment_code_to_string
 bp = Blueprint('actions', __name__, url_prefix='/actions')
 
 
-@bp.route('/', methods=['GET'])
-@login_required
-def actions():
-    pass
+@bp.route('/<int:page_num>', methods=['GET'])
+def actions(page_num):
+
+    actions_list = []
+    cursor = get_cursor()
+
+    cursor.execute("DECLARE @Status SMALLINT "
+                   "EXEC @Status = get_actions @Page_1=? "
+                   "SELECT @Status AS status", page_num)
+
+    try:
+        actions_list = cursor.fetchall()
+        cursor.nextset()
+        has_next = cursor.fetchval()
+        cursor.nextset()
+        status = cursor.fetchval()
+    except pyodbc.ProgrammingError:
+        status = actions_list[0][0]
+    if status == 0:
+        return render_template('actions/index.html', actions=actions_list, hasNext=has_next)
+    elif status == 1 or status == 2:
+        pass
+    else:
+        print("Unknown error code for get_party_members:", status)
+
+    return redirect(url_for('parties.parties'))
