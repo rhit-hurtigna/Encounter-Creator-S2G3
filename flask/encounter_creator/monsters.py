6abe2a52b32dd2f481f2208193cc9ffbf16a5a64
KingOfAllNerds <hajerjt@rose-hulman.edu>
2021-05-13 11:12:59 -0400

    monster added

--- flask/encounter_creator/monsters.py
+++ flask/encounter_creator/monsters.py
@@ -1,5 +1,5 @@
 """
-Nathan Hurtig - 4/16/21
+Jackson Hajer - 5/6/21
 from https://flask.palletsprojects.com/en/1.1.x/tutorial/blog/
 """
 from flask import (
@@ -13,7 +13,179 @@ from .db import get_cursor
 bp = Blueprint('monsters', __name__, url_prefix='/monsters')
 
 
-@bp.route('/', methods=('GET', 'POST'))
-@login_required
+@bp.route('/',methods=['GET'])
 def monsters():
-    return render_template('monsters/index.html', monsters=monsters)
+    cursor = get_cursor()
+    
+    cursor.execute("DECLARE @status SMALLINT EXEC @status = get_monster_info SELECT @status AS status")
+   
+    try:
+        monsters_list = cursor.fetchall()
+        status = cursor.fetchval()
+    except pyodbc.ProgrammingError:
+        status = 1
+        print('hi, bad things happened')
+        
+    return render_template('monsters/index.html', monsters=monsters_list)
+
+
+@bp.route('/create', methods=['POST'])
+@login_required
+def create():
+    name = request.form['name']
+    cr = request.form['cr']
+    type_name = request.form['type']
+    book_name = request.form['book']
+    alignment = request.form['alignment']
+
+    error = None
+    cursor = get_cursor()
+
+    
+    args = [name,cr]
+    query_str = 'DECLARE @status SMALLINT EXEC @status = create_monster @monst_name= ?, @CR= ?,'
+
+    if type_name != '':
+        args.append(type_name)
+        query_str = query_str + '@type_name = ?,'
+
+    if book_name != '':
+        args.append(book_name)
+        query_str = query_str + '@book_name = ?,'
+
+    if alignment != '':
+        args.append(alignment)
+        query_str = query_str + '@align = ?,'
+
+    query_str = query_str[:-1]
+    query_str = query_str + ' SELECT @status AS status'
+
+    cursor.execute(query_str, args)
+    status = cursor.fetchval()
+
+    if status == 0:
+        print('hi')
+        cursor.commit()
+        return redirect(url_for('monsters.monsters'))
+    elif status == 1:
+        print("Somehow, the MonsterID is null? Logging out")
+        flash("Sorry, something went wrong on our end.")
+        return redirect(url_for('auth.logout'))
+    elif status == 2:
+        print("Someone is trying to screw with our procedures!")
+        flash("Sorry, something went wrong on our end.")
+        return redirect(url_for('auth.logout'))
+    elif status == 3:
+        print("Safety check for null monster name should have triggered earlier in monster create view!")
+        error = 'Monster name is required.'
+    elif status == 4:
+        error = 'There is already a monster of that name!'
+    else:
+        print("Unknown error code in create_monster:", status)
+        error = 'Server error - try again?'
+    flash(error)
+    return redirect(url_for('monsters.create'))
+
+
+@bp.route('/search', methods=['GET','POST'])
+@login_required
+def search():
+    query = request.args['search']
+    if query == None or query == '':
+        return redirect(url_for('monsters.monsters'))
+    return redirect(url_for('monsters.searchView',name=query))
+
+@bp.route('/search/<name>', methods=['GET','POST'])
+def searchView(name):
+    cursor = get_cursor()
+    
+    cursor.execute("DECLARE @status SMALLINT EXEC @status = get_monster_info @name = ? SELECT @status AS status",name)
+    print(name)
+    try:
+        monsters_list = cursor.fetchall()
+        status = cursor.fetchval()
+    except pyodbc.ProgrammingError:
+        status = 1
+        print('hi, bad things happened')
+        
+    return render_template('monsters/index.html', monsters=monsters_list)
+
+
+@bp.route('/delete', methods=['POST'])
+def delete():
+    name = request.form['name']
+
+    error = None
+    cursor = get_cursor()
+
+    cursor.execute("DECLARE @status SMALLINT "
+                    "EXEC @status = delete_monster @monst_name = ? "
+                    "SELECT @status AS status", name)
+    status = cursor.fetchval()
+
+    if status == 0:
+        cursor.commit()
+        return redirect(url_for('monsters.monsters'))
+    elif status == 1:
+        print("Name cannot be null or empty.")
+        flash("Cannot delete an empty name")
+        return redirect(url_for('monsters.delete'))
+    elif status == 2:
+        print("Monster does not exist.")
+        flash("Cannot delete, monster doesn't exist")
+        return redirect(url_for('monsters.delete'))
+    else:
+        print("Unknown error code in delete_party:", status)
+        error = 'Server error - try again?'
+
+    flash(error)
+    return redirect(url_for('monsters.delete'))
+
+@bp.route('/edit', methods=['POST'])
+def edit():
+    name = request.form['name']
+    new_name = request.form['newName']
+    cr = request.form['cr']
+    type_name = request.form['type']
+    book_name = request.form['book']
+    alignment = request.form['alignment']
+
+    error = None
+    cursor = get_cursor()
+
+    args = [name]
+    query_str = "DECLARE @status SMALLINT EXEC @status = update_monster @monst_name= ?,"
+
+    if new_name != '':
+        args.append(new_name)
+        query_str = query_str + '@new_name = ?,'
+
+    if cr != '':
+        args.append(cr)
+        query_str = query_str + '@CR = ?,'
+
+    if type_name != '':
+            args.append(type_name)
+            query_str = query_str + '@type_name = ?,'
+
+    if book_name != '':
+        args.append(book_name)
+        query_str = query_str + '@book_name = ?,'
+
+    if alignment != '':
+        args.append(alignment)
+        query_str = query_str + '@align = ?,'
+    
+    query_str = query_str[:-1]
+    query_str = query_str + ' SELECT @status AS status'
+    print(query_str)
+    cursor.execute(query_str,args)
+    status = cursor.fetchval()
+
+    # TODO: add error checking
+    cursor.commit()
+    return redirect(url_for('monsters.monsters'))
+    
+
+    flash(error)
+    return redirect(url_for('monsters.monsters'))
