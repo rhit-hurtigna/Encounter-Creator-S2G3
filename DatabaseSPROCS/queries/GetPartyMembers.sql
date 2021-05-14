adfc85632acb0eb04d80a0768cd9c0268e76f29c
rhit-hurtigna <73034845+rhit-hurtigna@users.noreply.github.com>
2021-05-13 20:47:39 -0400

    Viewing owned books

--- DatabaseSPROCS/queries/GetPartyMembers.sql
+++ DatabaseSPROCS/queries/GetPartyMembers.sql
@@ -1,30 +1,29 @@
-CREATE PROCEDURE get_party_members
+CREATE PROCEDURE get_all_books
 (
-@ID_1 int
+@DMID_1 int
 )
 AS
 
 --  
--- Gets the members' IDs, names, classes,
--- levels, alignments, and races for
--- a party by the party's ID
+-- Gets all the books in the database,
+-- returning their IDs, names, and whether
+-- the DM currently 'has' them
 --
 -- Parameters:
--- ID_1: ID of the party in the party table
+-- DMID_1: ID of existing DM
 --
 -- Selects:
--- Rows containing (ID, name, class, level, alignment, race)
--- of party members
+-- Rows containing (ID, Name, Owned) for each
+-- book in the DB
 -------------------------------------------  
 -- Demo:  
 -- DECLARE @Status SMALLINT
--- EXEC @Status = get_party_members @ID_1 = 20
+-- EXEC @Status = get_all_books @ID_1 = 20
 -- SELECT Status = @Status
 -------------------------------------------  
 -- Revision History
--- Created 4/16/21 Nathan Hurtig
--- Altered 4/29/21 Nathan Hurtig to allow pyodbc
--- to access status codes on error
+-- Created 5/13/21 Nathan Hurtig
+--
 
 -- Turn NOCOUNT on because pydobc gets confused
 SET NOCOUNT ON
@@ -32,21 +31,21 @@ SET NOCOUNT ON
 -- Check that parameters are good  
 
 -- Is ID good?
-IF (@ID_1 is null)
+IF (@DMID_1 is null)
 BEGIN
   RETURN 1
 END
 
--- Check if party exists
-IF NOT EXISTS (SELECT * FROM Party WHERE ID=@ID_1)
+-- Check DM exists
+IF NOT EXISTS (SELECT * FROM DM WHERE ID=@DMID_1)
 BEGIN
   RETURN 2
 END
 
 
 -- Get values
-SELECT ID, Name, Class, Level, Alignment, Race
-FROM Member
-WHERE PartyID=@ID_1
+SELECT b.ID, b.Name, SIGN(ISNULL(db.BookID, 0)) AS Owned
+FROM Book b
+LEFT JOIN DMBooks db ON b.ID = db.BookID AND db.DMID = @DMID_1
 
 RETURN 0
--- flask/encounter_creator/main.py
+++ flask/encounter_creator/main.py
@@ -2,8 +2,9 @@
 Nathan Hurtig - 4/16/21
 from https://flask.palletsprojects.com/en/1.1.x/tutorial/blog/
 """
+import pyodbc
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, session
 )
 from werkzeug.exceptions import abort
 
@@ -19,7 +20,35 @@ def index():
     return render_template('main/index.html')
 
 
-@bp.route('/settings', methods=('GET', 'POST'))
+@bp.route('/settings', methods=['GET'])
 @login_required
 def settings():
-    return render_template('main/settings.html')
+    book_array = None
+    cursor = get_cursor()
+    cursor.execute("DECLARE @STATUS SMALLINT "
+                   "EXEC @STATUS = get_all_books @DMID_1=? "
+                   "SELECT @STATUS AS Status", session['user_id'])
+    try:
+        book_array = cursor.fetchall()
+        cursor.nextset()
+        status = cursor.fetchval()
+    except pyodbc.ProgrammingError:
+        status = book_array[0][0]
+    print(book_array)
+    if status == 0:
+        return render_template('main/settings.html', books=book_array)
+    elif status == 1 or status == 2:
+        print("Somehow, DM ID is bad? Logging out.")
+        flash("Sorry, something went wrong on our end.")
+        return redirect(url_for('auth.logout'))
+    else:
+        print("Unrecognized status code from get_all_books:", status)
+        flash("Sorry, something went wrong on our end.")
+        return redirect(url_for('main.index'))
+
+
+@bp.route('/books', methods=['POST'])
+@login_required
+def save_books():
+    print(request.form)
+    return redirect(url_for('main.settings'))
